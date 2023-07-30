da340a333274f8c8260337e58fb3f73970d6ffc5
Nolwenn <duflos.nol@laposte.net>
2023-07-30 22:56:07 +0200

    Leap years

--- script.js
+++ script.js
@@ -551,3 +551,18 @@ var max = function(list){
   
   return Math.max(...list)
 }
+
+//Leap years
+
+function isLeapYear(year) {
+  // TODO
+  if(year % 4 === 0 && year % 100 === 0 && year % 400 != 0){
+    return false;
+  }else if(year % 4 === 0){
+    return true;
+  }else if(year % 400 === 0){
+    return true;
+  } else if(year % 100 != 0){
+    return false;
+  }
+}
